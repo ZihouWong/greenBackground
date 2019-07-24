cc4ad3e82162b44927f3c201f0d61aa9139c7445
ZihouWong <wongzihou1997@gmail.com>
2019-07-24 10:19:32 +0800

    chore: create Page Model

---
+++ greenBackground/greenBackground/Page.swift
@@ -0,0 +1,15 @@
+//
+//  Page.swift
+//  greenBackground
+//
+//  Created by Zihou Wong on 7/24/19.
+//  Copyright © 2019 Zihou Wong. All rights reserved.
+//
+
+import UIKit
+
+struct Page {
+    let imageName: String
+    let headerText: String
+    let appendText: String
+}
